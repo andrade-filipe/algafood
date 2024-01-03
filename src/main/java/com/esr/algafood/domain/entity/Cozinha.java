dd96af119bb47d4bddd93f124ba9d1b2bc7b4cb6
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-03 11:51:13 -0300

    criando as primeiras entidades com javax persistence e lombok

---
+++ src/main/java/com/esr/algafood/domain/entity/Cozinha.java
@@ -0,0 +1,21 @@
+package com.esr.algafood.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder
+public class Cozinha {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String nome;
+}
---
+++ src/main/java/com/esr/algafood/domain/entity/Restaurante.java
@@ -0,0 +1,27 @@
+package com.esr.algafood.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder
+public class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String nome;
+
+    private BigDecimal taxaFrete;
+}

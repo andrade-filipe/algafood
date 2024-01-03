d4453fa911643c004b6006880f3446ee04f4367a
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-03 12:00:56 -0300

    builder precisa de allArgsConstructor para funcionar

--- src/main/java/com/esr/algafood/domain/entity/Cozinha.java
+++ src/main/java/com/esr/algafood/domain/entity/Cozinha.java
@@ -10,6 +10,7 @@ import javax.persistence.*;
 @RequiredArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Builder
+@AllArgsConstructor
 public class Cozinha {
 
     @Id
--- src/main/java/com/esr/algafood/domain/entity/Restaurante.java
+++ src/main/java/com/esr/algafood/domain/entity/Restaurante.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 @RequiredArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Builder
+@AllArgsConstructor
 public class Restaurante {
 
     @Id

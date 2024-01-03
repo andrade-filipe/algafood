97dd60429bb22c64dc16865a8aae63836e2d94e4
Filipe Andrade <filipeandrade.work@gmail.com>
2024-01-03 17:52:23 -0300

    exemplo de utilização do jpa

--- src/main/java/com/esr/algafood/jpa/CadastroCozinha.java
+++ src/main/java/com/esr/algafood/jpa/CadastroCozinha.java
@@ -2,6 +2,7 @@ package com.esr.algafood.jpa;
 
 import com.esr.algafood.domain.entity.Cozinha;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -16,5 +17,20 @@ public class CadastroCozinha {
 
     public List<Cozinha> listar(){
         return entityManager.createQuery("from Cozinha", Cozinha.class).getResultList();
+    }
+
+    public Cozinha buscar(Long id){
+        return entityManager.find(Cozinha.class, id);
+    }
+
+    @Transactional
+    public Cozinha adicionar(Cozinha cozinha){
+        return entityManager.merge(cozinha);
+    }
+
+    @Transactional
+    public void remover(Cozinha cozinha){
+        cozinha = buscar(cozinha.getId());
+        entityManager.remove(cozinha);
     }
 }
--- src/main/java/com/esr/algafood/jpa/CozinhaMain.java
+++ src/main/java/com/esr/algafood/jpa/CozinhaMain.java
@@ -2,14 +2,13 @@ package com.esr.algafood.jpa;
 
 import com.esr.algafood.AlgafoodApplication;
 import com.esr.algafood.domain.entity.Cozinha;
-import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.List;
 
-public class ConsultaCozinhaMain {
+public class CozinhaMain {
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApplication.class)
             .web(WebApplicationType.NONE)
@@ -17,10 +16,35 @@ public class ConsultaCozinhaMain {
 
         CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 
+        /*Consulta*/
         List<Cozinha> cozinhas = cadastroCozinha.listar();
 
         for (Cozinha cozinha : cozinhas) {
             System.out.println(cozinha.getNome());
         }
+
+        /*insert*/
+        Cozinha cozinha1 = new Cozinha();
+        cozinha1.setNome("Japonesa");
+
+        Cozinha cozinha2 = new Cozinha();
+        cozinha2.setNome("Italiana");
+
+        cadastroCozinha.adicionar(cozinha1);
+        cadastroCozinha.adicionar(cozinha2);
+
+        /*Busca*/
+        Cozinha cozinha = cadastroCozinha.buscar(1L);
+
+        /*Atualização*/
+
+        Cozinha cozinhaEdit = new Cozinha();
+        cozinhaEdit.setId(1L);
+        cozinhaEdit.setNome("Francesa");
+        cadastroCozinha.adicionar(cozinhaEdit);
+
+        /*Delete*/
+
+        cadastroCozinha.remover(cozinhaEdit);
     }
 }

edce0b5445868af8309dc0ddf08cfb55564822f7
Filipe Andrade <filipeandrade.work@gmail.com>
2024-03-24 00:21:00 -0300

    Post and Put methods :) FINALLY CAME BACK TO THE COURSE, OMG

--- src/main/java/com/esr/algafood/application/controller/CozinhaController.java
+++ src/main/java/com/esr/algafood/application/controller/CozinhaController.java
@@ -3,16 +3,17 @@ package com.esr.algafood.application.controller;
 import com.esr.algafood.domain.entity.Cozinha;
 import com.esr.algafood.domain.repository.CozinhaRepository;
 import com.esr.algafood.representation.model.xml.CozinhaXml;
+import com.fasterxml.jackson.databind.util.BeanUtil;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.Optional;
 
 import static java.util.Optional.*;
@@ -43,5 +44,23 @@ public class CozinhaController {
         }
 
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha adicionar(@RequestBody Cozinha cozinha){
+        return cozinhaRepository.save(cozinha);
+    }
+
+    @PutMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha){
+        try{
+            Cozinha currCozinha = cozinhaRepository.findById(cozinhaId).get();
+            BeanUtils.copyProperties(cozinha, currCozinha, "id");
+            cozinhaRepository.save(currCozinha);
+            return ResponseEntity.ok(currCozinha);
+        }catch (NoSuchElementException e){
+            return ResponseEntity.notFound().build();
+        }
     }
 }

b673e6a743e9857994e459542146e9dd3f7ace57
Filipe Andrade <filipeandrade.work@gmail.com>
2024-07-20 18:59:54 -0300

    trocando o repositório base por um customizado para melhor controle do código e mais desacoplamento

--- src/main/java/com/esr/algafood/AlgafoodApplication.java
+++ src/main/java/com/esr/algafood/AlgafoodApplication.java
@@ -1,9 +1,12 @@
 package com.esr.algafood;
 
+import com.esr.algafood.infrastructure.repository.CustomJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class AlgafoodApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AlgafoodApplication.class, args);
